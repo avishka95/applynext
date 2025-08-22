4dd673ce8c17690d1a6edcc7c9be7cb4d371e503
Avishka Ariyaratne <avishka95@gmail.com>
2025-08-22 14:36:50 +0200

    feat: add pg and adjust project for testing

--- services/job-scraper/jest.config.js
+++ services/job-scraper/jest.config.js
@@ -1,14 +1,49 @@
-const { createDefaultPreset } = require("ts-jest");
+const { createDefaultPreset } = require('ts-jest');
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' }
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {
+        ...tsJestTransformCfg
+    },
+    moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    projects: [
+        {
+            displayName: 'unit',
+            testMatch: ['**/__tests__/unit/**/*.test.ts'],
+            preset: 'ts-jest',
+            testEnvironment: 'node',
+            transform: {
+                ...tsJestTransformCfg
+            },
+            testMatch: ['**/__tests__/unit/**/*.test.ts'],
+            moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' }
+        },
+        {
+            displayName: 'integration',
+            testMatch: ['**/__tests__/integration/**/*.test.ts'],
+            preset: 'ts-jest',
+            testEnvironment: 'node',
+            transform: {
+                ...tsJestTransformCfg
+            },
+            testMatch: ['**/__tests__/integration/**/*.test.ts'],
+            moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' }
+        },
+        {
+            displayName: 'e2e',
+            testMatch: ['**/__tests__/e2e/**/*.test.ts'],
+            preset: 'ts-jest',
+            testEnvironment: 'node',
+            transform: {
+                ...tsJestTransformCfg
+            },
+            testMatch: ['**/__tests__/e2e/**/*.test.ts'],
+            moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' }
+        }
+    ]
 };
