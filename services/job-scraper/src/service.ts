d968cac941cd1459ad04baef5e10989913fa985f
Avishka Ariyaratne <avishka95@gmail.com>
2025-08-22 14:41:08 +0200

    chore: change service.ts file

--- services/job-scraper/src/service.ts
+++ services/job-scraper/src/service.ts
@@ -4,9 +4,9 @@ const service = express();
 service.use(express.json());
 
 service.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK' });
+    res.status(200).json({ status: 'OK' });
 });
 
-service.listen(3000, () => {
+export default service.listen(3000, () => {
     console.log('Job Scraper Service is running on port 3000');
 });
